963a0055ca7f012ea77ef4a55bb768d170aec64b
Birame SENE <bsene@pm.me>
2020-03-18 18:22:04 +0100

    finish dictionary project

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,5 +1,9 @@
 defmodule Dictionary do
+  def random_list(lst) do
+    lst |> word_list |> Enum.random
+  end
   def word_list(lst) do
-    []
+    lst
+    |> String.split("\n")
   end
 end
--- dictionary/test/dictionary_test.exs
+++ dictionary/test/dictionary_test.exs
@@ -2,7 +2,23 @@ defmodule DictionaryTest do
   use ExUnit.Case
   doctest Dictionary
 
-  test "word list should return empty list when no words qre provided" do
-    assert Dictionary.word_list('') == []
+  test "should return empty list when no words are provided" do
+    assert Dictionary.word_list("") == [""]
+  end
+
+  test "should return list with one element when one word is provided" do
+    assert Dictionary.word_list("toto") == ["toto"]
+  end
+  
+  test "should return list with two or more elements when at leat two words are provided" do
+    assert Dictionary.word_list("toto\npapa") == ["toto","papa"]
+    assert Dictionary.word_list("toto\npapa\nmaman") == ["toto","papa", "maman"]
+  end
+
+  test "should return randomized list" do
+    words = "toto\npapa\n"
+    assert Dictionary.random_list(words) != Dictionary.random_list(words)
+    words = "toto\npapa\nmaman"
+    assert Dictionary.random_list(words) != Dictionary.random_list(words)
   end
 end
