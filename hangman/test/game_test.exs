d0fc93f1a8c2aec34f21638a4891a3e999988701
Birame SENE <bsene@pm.me>
2020-03-25 08:35:53 +0100

    Hangman Game: check for duplicates moves

--- hangman/test/game_test.exs
+++ hangman/test/game_test.exs
@@ -11,15 +11,24 @@ defmodule GameTest do
     assert length(game.letters) > 0
   end
 
-  test "game instance letters is composed of lowercases ASCII characters" do
-    game = Game.new_game()
-    assert game.letters |> Enum.map(fn el -> String.match?(el, ~r/[a-z]/) end)
-  end
-
   test "state isn't changed for :won or :lost game" do
     for state <- [ :won, :lost ] do
       game = Game.new_game |> Map.put(:game_state, state)
       assert { game, _ } = Game.make_move(game, "x")
     end
+  end
+
+  test "first occurence of letter is not already used" do
+    game = Game.new_game
+    { game, _tally } = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+  end
+
+  test "second occurence of letter is not already used" do
+    game = Game.new_game
+    { game, _tally } = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+    { game, _tally } = Game.make_move(game, "x")
+    assert game.game_state == :already_used
   end
 end
